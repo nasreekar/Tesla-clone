445bf8102971e851337106fc83dbaf905ef3865c
Abhijith Sreekar <abhijith.sreekar@infra.tigerspike.com>
2021-07-22 21:11:23 +0530

    add snap animation on the flatlist

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FlatList, SafeAreaView } from 'react-native'
+import { FlatList, SafeAreaView, Dimensions } from 'react-native'
 import CarItem from '../CarItem'
 import cars from './cars'
 import styles from './styles'
@@ -11,6 +11,10 @@ const CarsList = () => {
                 data={cars}
                 renderItem={({ item }) => <CarItem car={item} />} //item is the item currently being rendered
                 keyExtractor={(item, index) => index.toString()}
+                showsVerticalScrollIndicator={false}
+                snapToAlignment={'start'}
+                decelerationRate={'fast'}  // the speed of animation when the items are moved up and down
+                snapToInterval={Dimensions.get('window').height}
             />
         </SafeAreaView>
     );
