e7b0e0f31fc744ca8a97206eadd2092aae5abd9c
Abhijith Sreekar <nasreekar@gmail.com>
2021-07-24 20:31:03 +0530

    downgrade jest version to run tests succesfullt

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -8,17 +8,8 @@ beforeEach(() => {
   });
 
 describe('<App />', () => {
-    it('sample test works', () => {
-        expect(1).toBe(1);
-    });
-
-    // it('renders the correct screen', async () => {
-    //         const { getByText } = render(<App />);
-    //         await waitFor(() => getByText('HomeScreen'));
-    //     });
-
-    // it('renders correctly', () => {
-    //     const { toJSON } = render(<App />);
-    //     expect(toJSON()).toMatchSnapshot();
-    //   });
+    it('renders correctly', () => {
+        const { toJSON } = render(<App />);
+        expect(toJSON()).toMatchSnapshot();
+      });
 });
