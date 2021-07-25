efce3d30525d2e59deae034d2f0bf5133f52f677
Abhijith Sreekar <nasreekar@gmail.com>
2021-07-25 22:00:18 +0530

    rename rendered to wrapper for custom button component in test

--- __tests__/components/CustomButton.test.js
+++ __tests__/components/CustomButton.test.js
@@ -17,30 +17,30 @@ describe("<CustomButton />", () => {
   const {color: buttonTextColor} = childStyles;
 
   it("renders correctly", () => {
-    const rendered = render(<CustomButton type = {buttonEnum.Primary} title={"order now"} onPress ={()=>({})} />);
-    expect(rendered).toMatchSnapshot();
+    const wrapper = render(<CustomButton type = {buttonEnum.Primary} title={"order now"} onPress ={()=>({})} />);
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("renders primary button correctly",() => {
-    const rendered = render(<CustomButton type = {buttonEnum.Primary} title={"order now"} onPress ={()=>({})} />);
-    const textComponent = rendered.getByTestId('customButtonTitle');
+    const wrapper = render(<CustomButton type = {buttonEnum.Primary} title={"order now"} onPress ={()=>({})} />);
+    const textComponent = wrapper.getByTestId('customButtonTitle');
     expect(textComponent.props.children).toEqual('order now');
   })
 
   it("renders secondary button correctly",() => {
-    const rendered = render(<CustomButton type = {buttonEnum.Secondary} title={"learn more"} onPress ={()=>({})} />);
-    const textComponent = rendered.getByTestId('customButtonTitle');
+    const wrapper = render(<CustomButton type = {buttonEnum.Secondary} title={"learn more"} onPress ={()=>({})} />);
+    const textComponent = wrapper.getByTestId('customButtonTitle');
     expect(textComponent.props.children).toEqual('learn more');
   })
 
-  it('primary button should be rendered in black color', () => {
+  it('primary button should be wrapper in black color', () => {
     const wrapper = render(<CustomButton type = {buttonEnum.Primary} title={"order now"} onPress ={()=>({})} />);
     const childStyles = wrapper.toJSON().children[0].props.style;
     const { backgroundColor } = childStyles;
     expect(backgroundColor).toEqual('#171A20CC');
   });
 
-  it('secondary button should be rendered in black color', () => {
+  it('secondary button should be wrapper in black color', () => {
     const wrapper = render(<CustomButton type = {buttonEnum.Secondary} title={"learn more"} onPress ={()=>({})} />);
     const childStyles = wrapper.toJSON().children[0].props.style;
     const { backgroundColor } = childStyles;
